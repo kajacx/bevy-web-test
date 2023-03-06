18aaf6ab1d5c034fed94fb27dbd2340c8a8d8e18
kajacx <kajacx@gmail.com>
2023-03-06 19:55:08 +0100

    Simple Bevy app with background color

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .insert_resource(ClearColor(Color::rgb(0.8, 0.4, 0.1)))
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
