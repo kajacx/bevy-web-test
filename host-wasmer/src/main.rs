a2dc9a0eaad0fa7b5fe4902f6b9eabe3bae09756
kajacx <kajacx@gmail.com>
2023-03-31 16:05:01 +0200

    Move wasmer host to separate folder

--- host-wasmer/src/main.rs
+++ host-wasmer/src/main.rs
@@ -3,7 +3,7 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(ClearColor(Color::rgb(0.9, 0.3, 0.1)))
+        .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.1)))
         .add_startup_system(setup)
         .run();
 }
