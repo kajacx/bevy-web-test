ee98134dbe85ac15aae2fc5788e97e1f711f30a1
kajacx <kajacx@gmail.com>
2023-03-31 19:36:48 +0200

    Incompatible export type?

--- host-wasmer/src/main.rs
+++ host-wasmer/src/main.rs
@@ -1,19 +1,22 @@
-use bevy::prelude::*;
+use bevy::{diagnostic::LogDiagnosticsPlugin, prelude::*};
 use wai_bindgen_wasmer::wasmer::*;
 
 const PLUGIN_BYTES: &'static [u8] =
-    include_bytes!("../../plugin/target/wasm32-unknown-unknown/debug/wai_sample_plugin.wasm");
+    include_bytes!("../../plugin/target/wasm32-unknown-unknown/debug/bevy_plugin.wasm");
 
 wai_bindgen_wasmer::import!("../protocol-plugin.wai");
 
 fn main() {
-    let plugin_color = get_color();
-    let bevy_color = Color::rgb(plugin_color.r, plugin_color.g, plugin_color.b);
+    // let plugin_color = get_color();
+    // let bevy_color = Color::rgb(plugin_color.r, plugin_color.g, plugin_color.b);
+    let bevy_color = Color::rgb(0.2, 0.5, 0.9);
 
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(LogDiagnosticsPlugin::default())
         .insert_resource(ClearColor(bevy_color))
         .add_startup_system(setup)
+        .add_system(print_color)
         .run();
 }
 
@@ -31,4 +34,8 @@ fn get_color() -> protocol_plugin::Color {
             .expect("should create instance");
 
     plugin.get_color(&mut store).expect("should get color")
+}
+
+fn print_color() {
+    info!("COLOR IS: {:?}", get_color());
 }
