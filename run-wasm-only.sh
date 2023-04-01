e3355fe9cb2fed0ad5a828fac8b20ee2264a7150
kajacx <kajacx@gmail.com>
2023-04-01 15:45:12 +0200

    gitignore pls

--- run-wasm-only.sh
+++ run-wasm-only.sh
@@ -2,8 +2,8 @@
 
 cd plugin && cargo build --target wasm32-unknown-unknown && cd ..  && \
 cd wasm-only && \
-wasm-pack build && \
-wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/debug/bevy-web-test.wasm && \
-cd out && docker-compose down && docker-compose up -d && cd .. && \
+wasm-pack build --target web && \
+# wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/debug/bevy-web-test.wasm && \
+cd pkg && docker-compose down && docker-compose up -d && cd .. && \
 cd .. && \
-echo "Done, view at http://localhost:8089"
+echo "Done, view at http://localhost:8091"
